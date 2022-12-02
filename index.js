ca12f70e279791872d934315d85b8836e948da6b
frans <franzferdinand.kbarek@gmail.com>
2022-12-02 18:57:17 +0900

    init commit

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-import express from "express";
-import dotenv from "dotenv";
-import parkingAreaData from "./routes/index.js";
+const express = require("express");
+const dotenv = require("dotenv");
+const parkingAreaRouter = require("./routes/index");
 
 dotenv.config();
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
-app.use("/api/parkingarea", parkingAreaData);
+app.use("/api", parkingAreaRouter);
 
-app.listen(5000, () => console.log("Connected to backend"));
+app.listen(5000, () => console.log("💐🌻🌿 Connected 🌹🦋🌴🌸"));
