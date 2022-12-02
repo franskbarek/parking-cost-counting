8c3621dc43d1686243aa60768367a3a23c59412f
frans <franzferdinand.kbarek@gmail.com>
2022-12-02 23:29:17 +0900

    _init commit

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const dotenv = require("dotenv");
+require("dotenv").config();
 const parkingAreaRouter = require("./routes/index");
 
-dotenv.config();
 const app = express();
 
 app.use(express.json());
