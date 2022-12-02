741227f0caa210ffbc29c7d30dabf5d4ac64ab72
frans <franzferdinand.kbarek@gmail.com>
2022-12-02 19:02:41 +0900

    allow .env var for codereviewer

--- config/postgreSql.js
+++
@@ -1,6 +0,0 @@
-import Knex from "knex";
-import { development } from "./knexfile.js";
-
-const db = Knex(development);
-
-export default db;
--- models/vehicle.models.js
+++
@@ -1,8 +0,0 @@
-import db from "../config/postgreSql.js";
-
-const vehicleModel = () => {
-  const res = db("vehicle");
-  return res;
-};
-
-export default vehicleModel;
